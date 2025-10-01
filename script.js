2d41908a2ce9df3be4cf38ae502ddd2d053fd9d1
jwviis <juliawyszynska24@wp.pl>
2025-10-01 13:58:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const tablica = [];
-const obiekt = {};
-const ciagZnakow = "hfgjdf";
-const numeryczny1 = 1;
-const numeryczny2 = 1.2;
-const logiczny = true;
-const nic = null;
-let niezdefiniowany;
+const tablica = [];   //tworzy zmienna o nazwie tablica i przypisuje pusta liniw
+const obiekt = {}; // tworzy zmienna obiekt 
+const ciagZnakow = "hfgjdf"; //tworzy zmienna tekstowa i zapisuje ten tekst 
+const numeryczny1 = 1; //tworzy zmienna i zapisuje liczbe calkowita 1
+const numeryczny2 = 1.2; //tworzy zmienan i zapisuje liczbe 1.2.
+const logiczny = true; // zmienna z wartoscia prawda 
+const nic = null; // zmienna ktora instnieje ale jest pusta 
+let niezdefiniowany; // istniee zmienna do ktorej nic sie nie wlozylo 
 const nodeElement = document.createElement('div');
 const nodeText = document.createTextNode('Hello, World!');
 nodeElement.appendChild(nodeText);
@@ -13,9 +13,9 @@ document.body.appendChild(nodeElement);
 
 
 
-const variables = [
-    { name: "tablica", value: tablica, type: Array.isArray(tablica) ? "Array" : typeof tablica },
-    { name: "obiekt", value: obiekt, type: typeof obiekt },
+const variables = [ // duza lista w ktorej zapisuje informacje o kazdym pudelku jak sie nazywa co w nim jest jakiego typu jest 
+    { name: "tablica", value: tablica, type: Array.isArray(tablica) ? "Array" : typeof tablica }, //  jest pudelko o nazwie tablica w srodku [] a jego typ to array 
+    { name: "obiekt", value: obiekt, type: typeof obiekt }, 
     { name: "ciagZnakow", value: ciagZnakow, type: typeof ciagZnakow },
     { name: "numeryczny1", value: numeryczny1, type: typeof numeryczny1 },
     { name: "numeryczny2", value: numeryczny2, type: typeof numeryczny2 },
@@ -31,9 +31,9 @@ const variables = [
 const table = document.getElementById("variablesTable");
 
 variables.forEach(v => {
-    const row = document.createElement("tr");
+    const row = document.createElement("tr"); //tworzy nowy wiersz
 
-    const cellName = document.createElement("td");
+    const cellName = document.createElement("td"); // dodaje kolumny nazwa zmiennej wartosc typ
     cellName.textContent = v.name;
     row.appendChild(cellName);
 
